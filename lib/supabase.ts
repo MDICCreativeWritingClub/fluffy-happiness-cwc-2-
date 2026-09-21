ca138685e7ba75b597793feac1b24bc9f060ef23
MDICCreativeWritingClub <mdiccreativewriting.developers@gmail.com>
2026-09-21 07:06:15 +0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wtlaacrgluajuuuskgub.supabase.co";
+const SUPABASE_URL = "https://yxbxunsqipswvrcfgrsm.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0bGFhY3JnbHVhanV1dXNrZ3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NDUzMTgsImV4cCI6MjEwNTAyMTMxOH0.8A6n78HcxYoBukL3Jsv7R-o1XpYrt09UuWds8y4RraM";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4Ynh1bnNxaXBzd3ZyY2ZncnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MTk5MzgsImV4cCI6MjEwMDI5NTkzOH0.8I0x0BgF0jvlYYlV01rs1L7qPWo-uRZT24HaS5JT7KU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
